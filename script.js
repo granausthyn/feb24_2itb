e2965b28ca3071494b1e4bd97444dff7db383327
Karl <Karladrian.solmiano.iics@ust.edu.ph>
2020-02-24 12:56:28 +0800

    fourth commit

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-//using object literal; directly assigning to a variable
-let person = { name: "gran de vera", gender: "male", age: 25 };
+//using ES6 Syntax
 
-console.log(`${person.name} is a ${person.gender} whose age is ${person.age}`);
+class Person{
+    constructor(name, gender, age){
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
 
-//using factory function
-
-function createPerson(name, gender, age) {
-    let p = {};
-    p.name = name;
-    p.gender = gender;
-    p.age = age;
+    walk(distance){
+        console.log(`${this.name} can walk up to ${distance} miles...`);
+    }
 }
 
-let gran = createPerson('Gran Austhyn De Vera', 'Male', 20);
-console.log(`${gran.name} is a ${gran.gender} whose age is ${gran.age}`);
-
-//using constructor function
-
-function Person(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-}
-
-let juan = new Person('Juan Dela Cruz', 'Male', 25);
-
-document.write(`${juan.age} si ${juan.name} at siya ay isang ${juan.gender}`);
+let granD = new Person('narG arev Ed','male', 20);
+granD.walk(5);

840a554e68dc118c508f6f5bb2992c23cbf3609e
Karl <Karladrian.solmiano.iics@ust.edu.ph>
2020-02-24 12:37:59 +0800

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-//using object literal
+//using object literal; directly assigning to a variable
 let person = {name:"gran de vera", gender:"male", age:25};
 
 console.log(`${person.name} is a ${person.gender} whose age is ${person.age}`);
+
+//using factory function
+
+function createPerson(name, gender, age){
+    let p = {};
+    p.name = name;
+    p.gender = gender;
+    p.age = age;
+}

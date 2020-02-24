27849dab1ef3626b036fa47a87c4bdaad19c3191
Karl <Karladrian.solmiano.iics@ust.edu.ph>
2020-02-24 13:11:46 +0800

    prototypes

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-//using ES6 Syntax
+//prototypes
 
-class Person{
-    constructor(name, gender, age){
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-    }
-
-    walk(distance){
-        console.log(`${this.name} can walk up to ${distance} miles...`);
-    }
+function Student(name, section, course){
+    this.name = name;
+    this.section = section;
+    this.course = course;
 }
 
-let granD = new Person('narG arev Ed','male', 20);
-granD.walk(5);
+let froilan = new Student("Froila Big Dick", "2 ITB", "IT");
+let gab = new Student("Gab Awesome Boy", "2 ITB", "IT");
+
+Student.prototype.school = "UST";
+Student.prototype.study = function(subject){
+    console.log(`${this.name} is enrolled in ${subject}...`);
+}
+
+//console.log(`${froilan.name} is studying in${froilan.school}`);
+froilan.study("ICS609");
